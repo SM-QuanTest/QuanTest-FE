addb2ae2195ad64eb713c1d03e6a8a64614f4747
Yerim Son <sonyerim12@gmail.com>
2025-07-08 21:39:47 +0900

    feat: 스플래시 화면 구현

--- app/src/main/java/com/example/quantest/MainActivity.kt
+++ app/src/main/java/com/example/quantest/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.quantest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.quantest.ui.theme.QuanTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +14,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             QuanTestTheme  {
-                MainScreen()
+                var showSplash by remember { mutableStateOf(true) }
+
+                if (showSplash) {
+                    SplashScreen(onTimeout = { showSplash = false })
+                } else {
+                    MainScreen()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/quantest/SplashScreen.kt
@@ -0,0 +1,38 @@
+package com.example.quantest
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun SplashScreen(
+    onTimeout: () -> Unit
+) {
+    LaunchedEffect(Unit) {
+        delay(2000) // 2초 대기 후 화면 전환
+        onTimeout()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = "앱 로고",
+            modifier = Modifier.size(120.dp)
+        )
+    }
+}
