e74502f2a75297e878839efca48eac204994ff9c
Yerim Son <sonyerim12@gmail.com>
2025-08-31 22:15:42 +0900

    style: 앱 컬러 토큰 정리

--- app/src/main/java/com/example/quantest/ui/theme/Color.kt
+++ app/src/main/java/com/example/quantest/ui/theme/Color.kt
@@ -10,6 +10,15 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF121212)
+
+val StormGray10 = Color(0xFFEDEDEF)
+val StormGray20 = Color(0xFFB0B8C1)
+val StormGray40 = Color(0xFF6B7684)
+val StormGray60 = Color(0xFF333D4B)
+val StormGray80 = Color(0xFF191F28)
+
 val Red = Color(0xFFF04452)
 val Orange = Color(0xFFFC8800)
 val Green = Color(0xFF02A262)
--- app/src/main/java/com/example/quantest/ui/theme/Theme.kt
+++ app/src/main/java/com/example/quantest/ui/theme/Theme.kt
@@ -10,17 +10,25 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = White,
+    primaryContainer = Black,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Black,
+    surface = Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = StormGray80,  // 선택 텍스트/아이콘
+    primaryContainer = White,   // 상단바/하단바 배경색
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = White,
+    surface = White,
+    onSurfaceVariant = StormGray40  // 비선택 텍스트/아이콘
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +45,7 @@ private val LightColorScheme = lightColorScheme(
 fun QuanTestTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
